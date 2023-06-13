6eeae1dce30de1b6fbc65ffbff226492e1a7ee32
CHADLIZakaria <zakaria.chadli05@gmail.com>
2023-06-14 00:22:54 +0100

    Add cors url environment

--- src/main/java/com/zchadli05/footStatsBack/config/CorsConfig.java
+++ src/main/java/com/zchadli05/footStatsBack/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.zchadli05.footStatsBack.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${CORS_URL}")
+    private String corsUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println(corsUrl);
         registry
-            .addMapping("/api/**")
-            .allowedOrigins("*")
+            .addMapping("/**")
+            .allowedOrigins(corsUrl)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
